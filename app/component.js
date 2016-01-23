5ce1f70fcd15d50437ff57ab9333dd292a8c4f57
Jim MacKenzie <protojazz@gmail.com>
2016-01-23 00:15:44 -0600

    Updated some config

--- app/component.js
+++ app/component.js
@@ -1,5 +1,5 @@
 module.exports = function() {
   var element = document.createElement('h1');
-  element.innerHTML = 'Hello FAR world';
+  element.innerHTML = 'Hello THE world';
   return element;
 };
